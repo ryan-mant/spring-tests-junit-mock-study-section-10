e1533820e32056e44d63a25a2d65efdd31205ca3
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-22 20:35:10 -0300

    test for find all list

--- src/test/java/br/com/erudio/controllers/PersonControllerTest.java
+++ src/test/java/br/com/erudio/controllers/PersonControllerTest.java
@@ -4,6 +4,7 @@ package br.com.erudio.controllers;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -23,6 +24,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.erudio.model.Person;
 import br.com.erudio.services.PersonServices;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @WebMvcTest
 class PersonControllerTest {
@@ -63,5 +67,26 @@ class PersonControllerTest {
                 .andExpect(jsonPath("$.firstName", is(person.getFirstName())))
                 .andExpect(jsonPath("$.lastName", is(person.getLastName())))
                 .andExpect(jsonPath("$.email", is(person.getEmail())));
+    }
+    @Test
+    @DisplayName("JUnit test for Given List Of Persons When FindAll Persons then Return Persons List")
+    void testGivenListOfPersons_WhenFindAllPersons_thenReturnPersonsList() throws Exception {
+
+        // Given / Arrange
+
+        List<Person> personList = new ArrayList<>();
+        personList.add(person);
+        personList.add(new Person("Maria", "Helena", "maria@email.com.br", "Rua 2", "female"));
+
+        given(service.findAll()).willReturn(personList);
+
+        // When / Act
+        ResultActions response = mockMvc.perform(get("/person"));
+
+        // Then / Assert
+        response.
+                andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.size()", is(personList.size())));
     }
 }
