2cad9be0354a2b1e9780d0144905d77d7c4c9c59
ryan-mant <ryan.tuglife@hotmail.com>
2025-07-25 20:23:36 -0300

    test for create

--- src/test/java/br/com/erudio/integrationtests/controllers/PersonControllerIntegrationTest.java
+++ src/test/java/br/com/erudio/integrationtests/controllers/PersonControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package br.com.erudio.integrationtests.controllers;
 import br.com.erudio.config.TestConfig;
 import br.com.erudio.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.erudio.model.Person;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.builder.RequestSpecBuilder;
@@ -12,6 +13,10 @@ import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
@@ -27,7 +32,7 @@ class PersonControllerIntegrationTest extends AbstractIntegrationTest {
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
 
         specification = new RequestSpecBuilder()
-                .setBasePath("")
+                .setBasePath("/person")
                 .setPort(TestConfig.SERVER_PORT)
                 .addFilter(new RequestLoggingFilter(LogDetail.ALL))
                 .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
@@ -37,10 +42,38 @@ class PersonControllerIntegrationTest extends AbstractIntegrationTest {
 
     // test[System Under Test]_[Condition or State Change]_[Expected Result]
     @DisplayName("Display Name")
+    @Order(1)
+    @DisplayName("JUnit integration test for Given Person Object when Create One Person Should Return A Person Object")
     @Test
-    void testABCD_when_XYZ_Should() {
-    	// Given / Arrange
-    	// When / Act
-    	// Then / Assert
+    void integrationTestGivenPersonObject_when_CreateOnePerson_ShouldReturnAPersonObject() throws JsonProcessingException {
+        var content = given()
+                .spec(specification)
+                .contentType(TestConfig.CONTENT_TYPE_JSON)
+                .body(person)
+                .when()
+                .post()
+                .then()
+                .statusCode(200)
+                .extract().body().asString();
+
+        Person savedPerson = mapper.readValue(content, Person.class);
+
+        person = savedPerson;
+
+        assertNotNull(savedPerson);
+        assertNotNull(savedPerson.getId());
+        assertNotNull(savedPerson.getFirstName());
+        assertNotNull(savedPerson.getLastName());
+        assertNotNull(savedPerson.getAddress());
+        assertNotNull(savedPerson.getGender());
+        assertNotNull(savedPerson.getEmail());
+
+        assertTrue(savedPerson.getId() > 0);
+        assertEquals("Lucas", savedPerson.getFirstName());
+        assertEquals("Pereira", savedPerson.getLastName());
+        assertEquals("Rua 1", savedPerson.getAddress());
+        assertEquals("male", savedPerson.getGender());
+        assertEquals("lucas@email.com.br", savedPerson.getEmail());
+
     }
 }
